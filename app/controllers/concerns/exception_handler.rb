c39ee524584fbf5e8c13a5b374355f69c36ac962
Siderlan Santos (Sider) <siderlan.santos@ferreri.co>
2020-12-04 13:55:18 -0300

    Add error handler centralized layer

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Handle common CRUD exceptions to send an appropriate message
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: { message: e.message }, status: :not_found
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      render json: { message: e.message }, status: :unprocessable_entity
+    end
+  end
+end
