e984803de4c1bb270aedee68049cc6ff62536081
Siderlan Santos (Sider) <siderlan.santos@ferreri.co>
2021-03-04 17:35:49 -0300

    Include new modules and add current_user helper function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+require 'json_web_token'
+
 class ApplicationController < ActionController::API
   include ExceptionHandler
+  include Secured
+  include Pundit
+
+  def current_user
+    identity_id = auth_token.first['session']['identity']['id']
+    @current_user ||= User.find_by(identity_id: identity_id)
+  end
 end
