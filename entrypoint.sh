260d57c38ae254106f044ad2cd760caf561d07f4
Siderlan Santos (Sider) <siderlan.santos@ferreri.co>
2021-05-19 10:38:06 -0300

    Move database migration to docker compose at local development

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /api/tmp/pids/server.pid
 
-# Create and migrate the database
-rails db:prepare
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
