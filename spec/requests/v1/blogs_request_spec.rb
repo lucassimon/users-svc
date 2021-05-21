6f95bf9f30e8a07eec6ebf8143b96858e7838b7f
Siderlan Santos (Sider) <siderlan.santos@ferreri.co>
2021-05-21 12:38:04 -0300

    Add new request tests for blogs API

--- spec/requests/v1/blogs_request_spec.rb
+++ spec/requests/v1/blogs_request_spec.rb
@@ -7,18 +7,35 @@ describe 'V1::Blog', type: :request, swagger_doc: 'v1/swagger.yaml' do
     post 'Creates a blog' do
       tags 'Blogs'
       consumes 'application/json'
-      parameter name: :blog, in: :body, schema: { '$ref' => '#components/schemas/new_blog' }
+      parameter name: :blog, in: :body, schema: { '$ref' => '#/components/schemas/new_blog' }
+      produces 'application/json'
+
       response '201', 'blog created' do
-        schema '$ref' => '#components/schemas/blog'
+        schema '$ref' => '#/components/schemas/blog'
 
         let(:blog) { attributes_for(:blog) }
         run_test!
       end
 
       response '422', 'invalid request' do
-        schema '$ref' => '#components/schemas/error_object'
+        schema '$ref' => '#/components/schemas/error_object'
 
         let(:blog) { { title: '' } }
+        run_test!
+      end
+    end
+
+    get 'Get the blogs list' do
+      tags 'Blogs'
+      produces 'application/json'
+
+      response '200', 'the blogs list' do
+        schema type: :array,
+               items: {
+                 '$ref' => '#/components/schemas/blog'
+               }
+
+        let(:blogs) { create_list(:blog, 10) }
         run_test!
       end
     end
@@ -27,20 +44,73 @@ describe 'V1::Blog', type: :request, swagger_doc: 'v1/swagger.yaml' do
   path '/v1/blogs/{id}' do
     get 'Retrieves a blog' do
       tags 'Blogs'
-      produces 'application/json'
       parameter name: :id, in: :path, type: :string
+      produces 'application/json'
 
-      response '200', 'blog found' do
-        schema '$ref' => '#components/schemas/blog'
+      response '200', 'the blog found' do
+        schema '$ref' => '#/components/schemas/blog'
 
         let(:id) { create(:blog).id }
         run_test!
       end
 
       response '404', 'blog not found' do
-        schema '$ref' => '#components/schemas/error_object'
+        schema '$ref' => '#/components/schemas/error_object'
 
         let(:id) { 0 }
+        run_test!
+      end
+    end
+
+    put 'Updates a blog' do
+      tags 'Blogs'
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :string
+      parameter name: :blog, in: :body, schema: { '$ref' => '#/components/schemas/new_blog' }
+      produces 'application/json'
+
+      let(:id) { create(:blog).id }
+      let(:blog) { { title: 'New title' } }
+
+      response '200', 'the updated blog' do
+        schema '$ref' => '#/components/schemas/blog'
+
+        run_test!
+      end
+
+      response '404', 'blog not found' do
+        schema '$ref' => '#/components/schemas/error_object'
+
+        let(:id) { 0 }
+
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        schema '$ref' => '#/components/schemas/error_object'
+
+        let(:blog) { { title: '' } }
+
+        run_test!
+      end
+    end
+
+    delete 'Removes a blog' do
+      tags 'Blogs'
+      parameter name: :id, in: :path, type: :string
+      produces 'application/json'
+
+      let(:id) { create(:blog).id }
+
+      response '204', 'Removes the blog successfuly' do
+        run_test!
+      end
+
+      response '404', 'blog not found' do
+        schema '$ref' => '#/components/schemas/error_object'
+
+        let(:id) { 0 }
+
         run_test!
       end
     end
