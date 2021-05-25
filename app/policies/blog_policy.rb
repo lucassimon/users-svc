1670d61e15a514a2311415de05506b7e74ea1e2c
Siderlan Santos (Sider) <siderlan.santos@ferreri.co>
2021-05-25 10:30:45 -0300

    Add blog policy

---
+++ app/policies/blog_policy.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class BlogPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(author_id: user.id)
+    end
+  end
+
+  def show?
+    owner?
+  end
+
+  def update?
+    owner?
+  end
+
+  def destroy?
+    owner?
+  end
+
+  private
+
+  def owner?
+    user.id == record.author_id
+  end
+end
