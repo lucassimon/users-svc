167a57783409cb5eb564b44a7ccf8bd68825b86e
Siderlan Santos (Sider) <siderlan.santos@ferreri.co>
2021-05-26 07:36:28 -0300

    Update security handling

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,9 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  describe 'current user' do
-    it 'raise NotImplementedError' do
-      expect { described_class.new.current_user }.to raise_error(NotImplementedError)
-    end
-  end
+  it { should respond_to(:current_user) }
 end
