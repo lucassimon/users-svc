3f41c1cb812c83ca5720b36412413254964ebfad
Lucas <lucassrod@gmail.com>
2021-06-15 15:36:36 -0300

    Identation

--- enginies/kratos/lib/kratos/session.rb
+++ enginies/kratos/lib/kratos/session.rb
@@ -16,69 +16,58 @@ module Kratos
     class HTTPRequestBadRequest < StandardError; end
   
     def initialize
-        super
-        @default_options = { headers: { Accept: 'application/json' } }
+      super
+      @default_options = { headers: { Accept: 'application/json' } }
     end
     
     def create_identity(body)
-        options = { 
-            headers: { 'Content-type' => 'application/json' }.merge!(@default_options[:headers]), 
-            body: body.to_json 
-        }
-        
-        response = self.class.post('/identities', options)
-  
-        raise_exception response unless response.code == 201
-  
-        # TODO: Change for the output::as_json::json_response
-        json_response response.body
+      options = { 
+          headers: { 'Content-type' => 'application/json' }.merge!(@default_options[:headers]), 
+          body: body.to_json 
+      }
+      
+      response = self.class.post('/identities', options)
+
+      raise_exception response unless response.code == 201
+
+      # TODO: Change for the output::as_json::json_response
+      json_response response.body
     end
   
     def get_identity(identity_id)
-        response = self.class.get("/identities/#{identity_id}", @default_options)
-        raise_exception response unless response.code == 200
-    
-        json_response response.body
+      response = self.class.get("/identities/#{identity_id}", @default_options)
+      raise_exception response unless response.code == 200
+  
+      json_response response.body
     end
   
     def delete_identity(identity_id)
-        response = self.class.delete("/identities/#{identity_id}", @default_options)
-        raise_exception response unless response.code == 204
+      response = self.class.delete("/identities/#{identity_id}", @default_options)
+      raise_exception response unless response.code == 204
     end
   
     # Source: https://www.ory.sh/kratos/docs/self-service/flows/user-registration/#api-clients
     # For API Clients, Ory Kratos responds with a JSON payload which includes the signed up identity:
     # Payload: { schema_id: 'default', "password": "foo_bar_123", traits: { 'email' => 'teste18@gmail.com' }}
     def register(payload)
-  
-        # TODO: Get the flow ID from the api client
-        response_flow = get_registration_api
-  
-        
-        
-        # TODO: Check errors
-        raise_exception response_flow unless response_flow.code == 200
-  
-        
-  
-        # TODO: Get the flow id
-        # the id is the same on the parameter ?flow_id='' on json['ui']['action']
-        flow_id = json_response(response_flow.body)[:id]
-  
-        
-  
-        # TODO: With the flow ID we can POST data
-        response_registration = post_registration_api(payload, flow_id)
-  
-        
-        
-        # TODO: Check errors
-        raise_exception response_registration unless response_registration.code == 200
-  
-        
-  
-        # TODO: Change for the output::as_json::json_response
-        json_response response_registration.body
+      # TODO: Get the flow ID from the api client
+      response_flow = get_registration_api
+
+      # TODO: Check errors
+      raise_exception response_flow unless response_flow.code == 200
+
+      # TODO: Get the flow id
+      # the id is the same on the parameter ?flow_id='' on json['ui']['action']
+      flow_id = json_response(response_flow.body)[:id]
+
+      # TODO: With the flow ID we can POST data
+      response_registration = post_registration_api(payload, flow_id)
+
+      # TODO: Check errors
+      raise_exception response_registration unless response_registration.code == 200
+
+      # TODO: Change for the output::as_json::json_response
+      json_response response_registration.body
     end
   
     # Example with curl
@@ -97,16 +86,16 @@ module Kratos
     #     }
     # }
     def get_registration_api()
-        options = { 
-            headers: { 'Content-type' => 'application/json' }.merge!(@default_options[:headers]) 
-        }
-        # TODO: Call the request
-        response = self.class.get('/self-service/registration/api', options)
-  
-        # TODO: Raise an expection here or let the register method dispatch it
-  
-        # TODO: Return the response
-        response
+      options = { 
+          headers: { 'Content-type' => 'application/json' }.merge!(@default_options[:headers]) 
+      }
+      # TODO: Call the request
+      response = self.class.get('/self-service/registration/api', options)
+
+      # TODO: Raise an expection here or let the register method dispatch it
+
+      # TODO: Return the response
+      response
     end
   
   
@@ -152,26 +141,30 @@ module Kratos
     # }
     # Payload: { schema_id: 'default', password: 'abcedarium', traits: { 'email' => 'teste17@gmail.com' }}
     def post_registration_api(payload, flow_id)
-        body = {'method': 'password'}.merge!(payload)
-  
-        options = { 
-            headers: { 'Content-type' => 'application/json' }.merge!(@default_options[:headers]),
-            query: { 'flow' => flow_id },
-            body: body.to_json 
-        }
-  
-        # TODO: Call the request
-        response = self.class.post('/self-service/registration', options)
+      body = {'method': 'password'}.merge!(payload)
 
-        # TODO: Raise an expection here or let the register method dispatch it
-  
-        # TODO: Return the response
-        response
+      options = { 
+          headers: { 'Content-type' => 'application/json' }.merge!(@default_options[:headers]),
+          query: { 'flow' => flow_id },
+          body: body.to_json 
+      }
+
+      # TODO: Call the request
+      response = self.class.post('/self-service/registration', options)
+
+      # TODO: Raise an expection here or let the register method dispatch it
+
+      # TODO: Return the response
+      response
     end
   
     # Source: https://www.ory.sh/kratos/docs/self-service/flows/user-login#api-clients
     # For API Clients, Ory Kratos responds with a JSON payload which includes the identity which just authenticated, the session, and the Ory Kratos Session Token:
     def login(payload)
+  
+    end
+
+    def recovery_password(payload)
   
     end
   
@@ -180,18 +173,18 @@ module Kratos
     end
   
     def json_response(body)
-        JSON.parse body, symbolize_names: true
+      JSON.parse body, symbolize_names: true
     end
   
     def raise_exception(response)
-        message = json_response(response.body)[:error][:message]
+      message = json_response(response.body)[:error][:message]
+
+      raise HTTPRequestBadRequest, message if response.code == 400
+      raise HTTPRequestNotFound, message if response.code == 404
+      raise HTTPRequestConflict, message if response.code == 409
+      raise HTTPRequestServerError, message if response.code == 500
   
-        raise HTTPRequestBadRequest, message if response.code == 400
-        raise HTTPRequestNotFound, message if response.code == 404
-        raise HTTPRequestConflict, message if response.code == 409
-        raise HTTPRequestServerError, message if response.code == 500
-    
-        raise HTTPRequestError, message
+      raise HTTPRequestError, message
     end
   end  
 end
